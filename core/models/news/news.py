9c215ab4c4503659397187a0f8a019bbb2deeb75
alexcpu7@gmail.com <Ja54So67N>
2019-05-21 23:23:14 +0300

    upgrade admin models

--- core/models/news/news.py
+++ core/models/news/news.py
@@ -1,10 +1,19 @@
+from uuid import uuid4
+from time import time
+
 from django.db import models
 from django.utils.html import format_html
 from django.utils.translation import ugettext_lazy as _
 from django.conf import settings
 
-from core.logic.save_file import UUIDFileStorage
 from core.models.news.attribute_news import NewsCategory, NewsTag
+
+
+def upload_path_handler_news(instance, filename):
+    import os.path
+    fn, ext = os.path.splitext(filename)
+    file_name = '{}_{}{}'.format(uuid4().hex, int(time()), ext)
+    return "news/news/{}/images/{}".format(instance.url, file_name)
 
 
 class News(models.Model):
@@ -26,18 +35,14 @@ class News(models.Model):
 
     preview_title = models.CharField(_('Загловок (анонс)'), max_length=100)
     preview_image = models.ImageField(_('Изображение (анонс)'),
-                                      upload_to='news/new__{}/preview_images'.format(url),
-                                      storage=UUIDFileStorage(),
-                                      unique=True,
-                                      blank=True, null=True)
+                                      upload_to=upload_path_handler_news,
+                                      unique=True, blank=True, null=True)
     preview_desc = models.TextField(_('Описание (анонс)'), blank=True, null=True)
 
     title = models.CharField(_('Заголовок'), max_length=100)
     image = models.ImageField(_('Изображение'),
-                              upload_to='news/new__{}/images'.format(url),
-                              storage=UUIDFileStorage(),
-                              unique=True,
-                              blank=True, null=True)
+                              upload_to=upload_path_handler_news,
+                              unique=True, blank=True, null=True)
     desc = models.TextField(_('Контент'))
 
     tag = models.ManyToManyField(NewsTag, verbose_name=_('Теги'), blank=True)
